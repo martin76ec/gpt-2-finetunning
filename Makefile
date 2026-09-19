f9652ea4e49e3bd07ff1d7cf1598c0c8200c9ba7
martin76ec <larreaamartin@gmail.com>
2026-09-19 11:29:18 -0500

    feat: add raw copy paste of the notebook feat: started the organization of the notebook

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	python src/models/mini_gpt.py
---
+++ src/pipelines/train.py
@@ -0,0 +1,116 @@
+import json
+from functools import partial
+
+import tiktoken
+import torch
+from torch.utils.data import DataLoader
+
+from src.dataset.instruction import InstructionDataset
+
+num_workers = 0
+batch_size = 8
+
+torch.manual_seed(123)
+
+
+def load_data():
+    data = None
+    with open("data/instruction-data.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
+
+
+data = load_data()
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+train_portion = int(len(data) * 0.85)  # 85% for training
+test_portion = int(len(data) * 0.1)  # 10% for testing
+val_portion = len(data) - train_portion - test_portion  # Remaining 5% for validation
+train_data = data[:train_portion]
+test_data = data[train_portion : train_portion + test_portion]
+val_data = data[train_portion + test_portion :]
+
+
+def custom_collate_fn(
+    batch, pad_token_id=50256, ignore_index=-100, allowed_max_length=None, device="gpu"
+):
+    # Find the longest sequence in the batch
+    batch_max_length = max(len(item) + 1 for item in batch)
+
+    # Pad and prepare inputs and targets
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        # Add an <|endoftext|> token
+        new_item += [pad_token_id]
+        # Pad sequences to max_length
+        padded = new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        inputs = torch.tensor(padded[:-1])  # Truncate the last token for inputs
+        targets = torch.tensor(padded[1:])  # Shift +1 to the right for targets
+
+        # New: Replace all but the first padding tokens in targets by ignore_index
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        # New: Optionally truncate to maximum sequence length
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    # Convert list of inputs and targets to tensors and transfer to target device
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+
+    return inputs_tensor, targets_tensor
+
+
+customized_collate_fn = partial(
+    custom_collate_fn, device="gpu", allowed_max_length=1024
+)
+
+train_dataset = InstructionDataset(train_data, tokenizer)
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=batch_size,
+    collate_fn=customized_collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=num_workers,
+)
+
+val_dataset = InstructionDataset(val_data, tokenizer)
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=batch_size,
+    collate_fn=customized_collate_fn,
+    shuffle=False,
+    drop_last=False,
+    num_workers=num_workers,
+)
+
+
+torch.manual_seed(123)
+
+
+def format_input(entry):
+    instruction_text = (
+        f"Below is an instruction that describes a task. "
+        f"Write a response that appropriately completes the request."
+        f"\n\n### Instruction:\n{entry['instruction']}"
+    )
+
+    input_text = f"\n\n### Input:\n{entry['input']}" if entry["input"] else ""
+
+    return instruction_text + input_text
+
+
+input_text = format_input(val_data[0])
+print(input_text)
