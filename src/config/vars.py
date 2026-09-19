febbcf584d0d1fb5ec41e55a944c3988eb967199
martin76ec <larreaamartin@gmail.com>
2026-09-19 14:30:51 -0500

    feat: add vars

---
+++ src/config/vars.py
@@ -0,0 +1,15 @@
+NUM_WORKERS = 4
+
+BATCH_SIZE = 8
+
+DEVICE = "cuda"
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,  # Vocabulary size
+    "context_length": 1024,  # Shortened context length (orig: 1024)
+    "emb_dim": 768,  # Embedding dimension
+    "n_heads": 12,  # Number of attention heads
+    "n_layers": 12,  # Number of layers
+    "drop_rate": 0.1,  # Dropout rate
+    "qkv_bias": True,  # Query-key-value bias
+}
